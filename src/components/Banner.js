c5b965f647aaa356dd83e997b4585f93f6c3dd9d
Rita Guilherme <rita@Ritas-MacBook-Air.local>
2023-02-27 16:41:48 +0000

    27.02

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,30 +10,38 @@ function Banner() {
 
       <div className="grid grid-cols-2  gap-7 p-20  ">
         {/* 1 box */}
-        <div className="row-span-3 bg-[#DDDDDD]/20 rounded-lg shadow-lg z-5">
-          <div className="relative bg-[#797A7E]/60 rounded-t-lg  ">
-            <p className="p-1 text-left text-white">About me</p>{" "}
+        <div className="row-span-3 bg-[#FFFFFF] rounded-lg shadow-lg z-5 drop-shadow-2xl ">
+          <div className="relative bg-gradient-to-b from-[#DFDFDE] rounded-t-lg p-2 pb-10 ">
+            <p className="p-1 text-left text-[#595260] font-semibold">
+              About me
+            </p>{" "}
           </div>
           box 1
         </div>
         {/* 2 box */}
-        <div className="row-span-1 bg-[#DDDDDD]/20 rounded-lg shadow-lg z-5">
-          <div className="relative bg-[#797A7E]/60 rounded-t-lg  ">
-            <p className="p-1 text-left text-white">Skills</p>{" "}
+        <div className="row-span-1 bg-[#FFFFFF] rounded-lg shadow-lg z-5 drop-shadow-2xl">
+          <div className="relative bg-gradient-to-b from-[#DFDFDE] rounded-t-lg p-2 pb-10 ">
+            <p className="p-1 text-left  text-[#595260] font-semibold">
+              Skills
+            </p>{" "}
           </div>
           <p className="p-14"> box 2</p>
         </div>
         {/* 3 Box */}
-        <div className="row-span-2 bg-[#DDDDDD]/20 rounded-lg shadow-lg z-5">
-          <div className="relative bg-[#797A7E]/60 rounded-t-lg  ">
-            <p className="p-1 text-left text-white">GitHub</p>{" "}
+        <div className="row-span-2 bg-[#FFFFFF] rounded-lg shadow-lg z-5 drop-shadow-2xl">
+          <div className="relative bg-gradient-to-b from-[#DFDFDE] rounded-t-lg p-2 pb-10  ">
+            <p className="p-1 text-left  text-[#595260] font-semibold">
+              GitHub
+            </p>{" "}
           </div>
           <p className="p-24"> box 3</p>
         </div>
         {/* 4 box */}
-        <div className="col-span-2 bg-[#FBF7F0] rounded-lg shadow-lg z-5">
-          <div className="relative bg-[#797A7E]/60  rounded-t-lg  ">
-            <p className="p-1 text-left text-white">Work Experience</p>{" "}
+        <div className="col-span-2 bg-[#FFFFFF] rounded-lg shadow-lg z-5 drop-shadow-2xl">
+          <div className="relative bg-gradient-to-b from-[#DFDFDE] rounded-t-lg p-2 pb-10  ">
+            <p className="p-1 text-left  text-[#595260] font-semibold">
+              Work Experience
+            </p>{" "}
           </div>
           <p className="p-10">box 4</p>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-[#DDDDDD]">
+      <main className="bg-[#FFFFFF]">
         <Header />
 
         <Banner />
