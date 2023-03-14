6ff2d5fb05454fee07ad5af6fff8f9172f59c368
Rita Guilherme <rita@Ritas-MacBook-Air.local>
2023-03-14 16:12:45 +0000

    V14.03.7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
@@ -7,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        hero: "url('../../public/images/OrangeWP.png')",
+        hero: "url('https://live.staticflickr.com/65535/52747212546_b7a6c7c713_b.jpg')",
       },
     },
   },
